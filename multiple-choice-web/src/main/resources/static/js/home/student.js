51926a904748f70583966f9a17b11881d8cf0c86
SGTSuPo <105306770+SGTSuPo@users.noreply.github.com>
2024-06-08 03:17:53 +0700

    Update student.js

--- multiple-choice-web/src/main/resources/static/js/home/student.js
+++ multiple-choice-web/src/main/resources/static/js/home/student.js
@@ -1,39 +1,3 @@
-async function fetchData() {
-  try {
-    const response = await fetch("http://127.0.0.1:5500/frontend/class.json");
-    if (!response.ok) {
-      throw new Error("Network response was not ok " + response.statusText);
-    }
-    const data = await response.json();
-    const classes = data.class;
-    const sidebar = document.getElementById("sidebar");
-    const workspace = document.getElementById("workspace");
-    classes.forEach((item) => {
-      //sidebar
-      sidebar.insertAdjacentHTML('beforeend',`<a href="joinClass.html?id=${item.id}" class="btn btn-secondary">${item.name}</a>`)
-      //room
-      workspace.insertAdjacentHTML(
-        "beforeend",
-        `<div class="m-1 room">
-          <div class="card-title">
-              <a href="joinClass.html?id=${item.id}"><div class="d-flex justify-content-center roomname">${item.name}</div></a>
-              <hr class="m-0 bg-light">
-              <div class="info-class p-2">
-                <div class="info text-light">Subject: ${item.subject}</div>
-                <div class="info text-light">Grade: ${item.grade}</div>
-                <div class="info text-light">Teacher: ${item.host.name}</div>
-                <div class="info text-light">Code: ${item.id}</div>
-              </div>
-            </div>
-        </div>`
-      );
-    });
-  } catch (error) {
-    console.error("There has been a problem with your fetch operation:", error);
-  }
-}
-fetchData();
-// join
 document
   .getElementById("join-class-btn")
   .addEventListener("click", function (event) {
